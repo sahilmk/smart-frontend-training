6d09b1f450537fae1553629c9fe073b5b9a5b7a3
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-03 10:09:26 +0530

    #9 Solve the error of the negative remainder

--- javascript/javascript exercise/program-2/main.js
+++ javascript/javascript exercise/program-2/main.js
@@ -5,13 +5,17 @@ function getQuotientReminder() {
     var divisor = parseFloat(document.getElementById('divisor').value);
 
     if (divisor != 0 && !isNaN(dividend) && !isNaN(divisor)) {
-        let quotient = (dividend / divisor).toFixed(2);
+        let quotient = Math.floor(dividend / divisor);
 
 
-        var reminder = (dividend - (divisor * Math.floor(quotient)));
-
-
-        getMessage(`quotient: ${quotient} and Reminder: ${reminder}`);
+        var reminder = (dividend - (divisor * quotient));
+        var reminder2;
+        if (divisor < 0) {
+            reminder2 = reminder - divisor;
+            getMessage(`quotient: ${quotient} and Reminder: ${reminder} and also ${reminder2}`);
+        } else {
+            getMessage(`quotient: ${quotient} and Reminder: ${reminder}`);
+        }
     }
     else {
         getMessage('Please Enter valid input');
