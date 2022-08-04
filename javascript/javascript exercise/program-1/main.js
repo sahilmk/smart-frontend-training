d28dfa5c9a91dc41a46b4f47a58659a16cf3059f
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-04 17:27:30 +0530

    #8 Remove the two forms and created a one form for input

--- javascript/javascript exercise/program-1/main.js
+++ javascript/javascript exercise/program-1/main.js
@@ -1,74 +1,90 @@
 document.querySelector('#submit1').addEventListener('click', doOperation);
-document.querySelector('#submit2').addEventListener('click', squareOrQube);
+
+
+document.getElementById('select1').addEventListener('click', disable);
+
+function disable(e) {
+    operator1 = document.getElementById('select1').value;
+    if (operator1 === 'square' || operator1 === 'cube') {
+        document.getElementById('input2').style.display = 'none';
+        document.querySelector('label[for=input2]').style.display = 'none';
+    }
+    else {
+        document.getElementById('input2').style.display = 'inline';
+        document.querySelector('label[for=input2]').style.display = 'inline';
+    }
+    e.preventDefault();
+}
+
 
 
 function doOperation(e) {
-    number1 = document.getElementById('input1').value;
-    number2 = document.getElementById('input2').value;
     operator1 = document.getElementById('select1').value;
+    if (operator1 !== 'square' && operator1 !== 'cube') {
+        number1 = document.getElementById('input1').value;
+        number2 = document.getElementById('input2').value;
+        if (number1 != '' && number2 != '') {
+            number1 = Number(number1);
+            number2 = Number(number2);
+            switch (operator1) {
+                case 'addition':
+                    document.getElementById('show').innerText = `Addition of ${number1} and ${number2} is: ${number1 + number2}`;
+                    break;
+                case 'subtraction':
+                    document.getElementById('show').innerText = `Subtraction of ${number1} and ${number2} is: ${number1 - number2}`;
+                    break;
 
-    if (!isNaN(Number(number1)) && !isNaN(Number(number2)) && number1 != '' && number2 != '') {
-        number1 = Number(number1);
-        number2 = Number(number2);
-        switch (operator1) {
-            case 'addition':
-                document.getElementById('show').innerText = `Addition of ${number1} and ${number2} is: ${number1 + number2}`;
-                break;
-            case 'subtraction':
-                document.getElementById('show').innerText = `Subtraction of ${number1} and ${number2} is: ${number1 - number2}`;
-                break;
+                case 'multiplication':
+                    document.getElementById('show').innerText = `Multiplication of ${number1} and ${number2} is: ${number1 * number2}`;
+                    break;
 
-            case 'multiplication':
-                document.getElementById('show').innerText = `Multiplication of ${number1} and ${number2} is: ${number1 * number2}`;
-                break;
+                case 'division':
+                    if (number2 === 0) {
+                        document.getElementById('show').innerText = 'Plese enter valid input';
+                    }
+                    else {
+                        document.getElementById('show').innerText = `Division of ${number1} and ${number2} is: ${number1 / number2}`;
+                    }
+                    break;
 
-            case 'division':
-                if (number2 === 0) {
-                    document.getElementById('show').innerText = 'Plese enter valid input';
-                }
-                else {
-                    document.getElementById('show').innerText = `Division of ${number1} and ${number2} is: ${number1 / number2}`;
-                }
+                case 'remainder':
+                    if (number2 === 0) {
+                        document.getElementById('show').innerText = 'Plese enter valid input';
+                    }
+                    else {
+                        document.getElementById('show').innerText = `Remainder of ${number1} and ${number2} is: ${number1 % number2}`;
+                    }
+                    break;
 
-                break;
+            }
+        } else {
+            document.getElementById('show').innerText = 'Plese enter valid input';
+        }
 
-            case 'remainder':
-                if (number2 === 0) {
-                    document.getElementById('show').innerText = 'Plese enter valid input';
-                }
-                else {
-                    document.getElementById('show').innerText = `Remainder of ${number1} and ${number2} is: ${number1 % number2}`;
-                }
+    } else {
 
-                break;
-
+        number1 = document.getElementById('input1').value;
+        if (number1 != '') {
+            number1 = Number(number1);
+            switch (operator1) {
+                case 'square':
+                    document.getElementById('show').innerText =
+                        `Square of ${number1} is: ${Math.pow(number1, 2)}`;
+                    break;
+                case 'cube':
+                    document.getElementById('show').innerText = `Cube of ${number1} is: ${Math.pow(number1, 3)}`;
+                    break;
+            }
+        }
+        else {
+            document.getElementById('show').innerText = 'Plese enter valid input';
         }
     }
-    else {
-        document.getElementById('show').innerText = 'Plese enter valid input';
-    }
+
+
+    number1 = document.getElementById('input1').value = '';
+    number2 = document.getElementById('input2').value = '';
     e.preventDefault();
 }
 
-function squareOrQube(e) {
-    number3 = document.getElementById('input3').value;
-    operator2 = document.getElementById('select2').value;
 
-    if (!isNaN(Number(number3)) && number3 != '') {
-        number3 = Number(number3);
-        switch (operator2) {
-            case 'square':
-                document.getElementById('show').innerText =
-                    `Square of ${number3} is: ${Math.pow(number3, 2)}`;
-                break;
-            case 'cube':
-                document.getElementById('show').innerText = `Cube of ${number3} is: ${Math.pow(number3, 3)}`;
-                break;
-        }
-    }
-    else {
-        document.getElementById('show').innerText = 'Plese enter valid input';
-    }
-
-    e.preventDefault();
-}
