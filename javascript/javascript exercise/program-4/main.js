791df16613a715bb05f98a07ef9111c14b3a03f8
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-04 18:16:22 +0530

    #11 Done the practical 4 of the js practical list

---
+++ javascript/javascript exercise/program-4/main.js
@@ -0,0 +1,22 @@
+document.getElementById('submit').addEventListener('click', findOddEven);
+
+function findOddEven(e) {
+    number = document.getElementById('input').value;
+    if (number !== '') {
+        number = Number(number)
+        if ((number % 2) == 0) {
+            showMessage(`${number} is even number.`);
+        } else {
+            showMessage(`${number} is odd number.`);
+        }
+    } else {
+        showMessage('Please enter a number');
+    }
+
+
+    e.preventDefault();
+}
+
+function showMessage(message) {
+    document.getElementById('show').innerText = message;
+}
