72295e42d5d1b3834b0c0149ead3ab9f8a59df2b
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-05 11:06:11 +0530

    #8 Done the changes according to the review, add the function for the message, and update the error messages

--- javascript/javascript exercise/program-1/main.js
+++ javascript/javascript exercise/program-1/main.js
@@ -1,90 +1,96 @@
 document.querySelector('#submit1').addEventListener('click', doOperation);
+document.getElementById('select1').addEventListener('click', disableOneInput);
 
+//Diable the second input when user want calculate the cube and square
+function disableOneInput(e) {
+    const operator1 = document.getElementById('select1').value;
 
-document.getElementById('select1').addEventListener('click', disable);
-
-function disable(e) {
-    operator1 = document.getElementById('select1').value;
     if (operator1 === 'square' || operator1 === 'cube') {
         document.getElementById('input2').style.display = 'none';
         document.querySelector('label[for=input2]').style.display = 'none';
-    }
-    else {
+    } else {
         document.getElementById('input2').style.display = 'inline';
         document.querySelector('label[for=input2]').style.display = 'inline';
     }
+
     e.preventDefault();
 }
 
 
 
 function doOperation(e) {
-    operator1 = document.getElementById('select1').value;
+    const operator1 = document.getElementById('select1').value;
+
     if (operator1 !== 'square' && operator1 !== 'cube') {
-        number1 = document.getElementById('input1').value;
-        number2 = document.getElementById('input2').value;
+        let number1 = document.getElementById('input1').value;
+        let number2 = document.getElementById('input2').value;
+
         if (number1 != '' && number2 != '') {
+            //Conver the string to the number
             number1 = Number(number1);
             number2 = Number(number2);
+
+            //Calculate the addition, subtraction, division, multiplicatoin and remainder.
             switch (operator1) {
                 case 'addition':
-                    document.getElementById('show').innerText = `Addition of ${number1} and ${number2} is: ${number1 + number2}`;
+                    showMessage(`Addition of ${number1} and ${number2} is: ${number1 + number2}`);
                     break;
                 case 'subtraction':
-                    document.getElementById('show').innerText = `Subtraction of ${number1} and ${number2} is: ${number1 - number2}`;
+                    showMessage(`Subtraction of ${number1} and ${number2} is: ${number1 - number2}`);
                     break;
 
                 case 'multiplication':
-                    document.getElementById('show').innerText = `Multiplication of ${number1} and ${number2} is: ${number1 * number2}`;
+                    showMessage(`Multiplication of ${number1} and ${number2} is: ${number1 * number2}`);
                     break;
 
                 case 'division':
                     if (number2 === 0) {
-                        document.getElementById('show').innerText = 'Plese enter valid input';
-                    }
-                    else {
-                        document.getElementById('show').innerText = `Division of ${number1} and ${number2} is: ${number1 / number2}`;
+                        showMessage('0 as divisor not allowed please enter  non-zeor value');
+                    } else {
+                        showMessage(`Division of ${number1} and ${number2} is: ${(number1 / number2).toFixed(2)}`);
                     }
                     break;
 
                 case 'remainder':
                     if (number2 === 0) {
-                        document.getElementById('show').innerText = 'Plese enter valid input';
-                    }
-                    else {
-                        document.getElementById('show').innerText = `Remainder of ${number1} and ${number2} is: ${number1 % number2}`;
+                        showMessage('Plese enter valid input');
+                    } else {
+                        showMessage(`Remainder of ${number1} and ${number2} is: ${number1 % number2}`);
                     }
                     break;
-
             }
         } else {
-            document.getElementById('show').innerText = 'Plese enter valid input';
+            showMessage('Please enter the both number');
         }
 
     } else {
+        let number1 = document.getElementById('input1').value;
 
-        number1 = document.getElementById('input1').value;
+        //Calculate the square and cube
         if (number1 != '') {
             number1 = Number(number1);
+
             switch (operator1) {
                 case 'square':
-                    document.getElementById('show').innerText =
-                        `Square of ${number1} is: ${Math.pow(number1, 2)}`;
+                    showMessage(`Square of ${number1} is: ${Math.pow(number1, 2)}`);
                     break;
                 case 'cube':
                     document.getElementById('show').innerText = `Cube of ${number1} is: ${Math.pow(number1, 3)}`;
                     break;
             }
-        }
-        else {
-            document.getElementById('show').innerText = 'Plese enter valid input';
+        } else {
+            showMessage('Plese enter the value');
         }
     }
 
 
-    number1 = document.getElementById('input1').value = '';
-    number2 = document.getElementById('input2').value = '';
+    //Clear the inputs after user click the submit button
+    document.getElementById('input1').value = '';
+    document.getElementById('input2').value = '';
     e.preventDefault();
 }
 
-
+//Show the error messag and also the output.
+function showMessage(message) {
+    document.getElementById('show').innerText = message;
+}
