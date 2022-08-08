47bbf5d3250bf63d86d0c8dceed477603059b2f4
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-08 13:43:02 +0530

    #11 Completed the practical 11

--- javascript/javascript exercise/program-4/main.js
+++ javascript/javascript exercise/program-4/main.js
@@ -1,26 +1,39 @@
 document.getElementById('submit').addEventListener('click', findOddEven);
+document.getElementById('input').addEventListener('keyup', validateInput);
 
 function findOddEven(e) {
-
-    number = document.getElementById('input').value;
+    let number = document.getElementById('input').value;
 
     if (number !== '') {
         number = Number(number)
 
         if ((number % 2) == 0) {
-            showMessage(`${number} is even number.`);
+            showMessage('show', `${number} is even number.`);
         } else {
-            showMessage(`${number} is odd number.`);
+            showMessage('show', `${number} is odd number.`);
         }
 
     } else {
-        showMessage('Please enter a number');
+        showMessage('error', 'Please enter a number');
     }
 
+    clearInput();
 
     e.preventDefault();
 }
 
-function showMessage(message) {
-    document.getElementById('show').innerText = message;
+function validateInput(e) {
+    if (isNaN(Number(e.target.value))) {
+        showMessage('error', 'Only digit is allowed')
+    } else {
+        document.getElementById('error').innerText = '';
+    }
 }
+
+function showMessage(targetId, message) {
+    document.getElementById(targetId).innerText = message;
+}
+
+function clearInput() {
+    document.getElementById('input').value = '';
+} 
