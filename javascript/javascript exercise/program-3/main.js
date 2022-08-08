bc7bef7a3c5f5b5ba7686daf7a520083fabd874c
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-08 12:04:22 +0530

    #10 Done the practical 3

--- javascript/javascript exercise/program-3/main.js
+++ javascript/javascript exercise/program-3/main.js
@@ -1,23 +1,24 @@
 document.getElementById('swap').addEventListener('click', swapNubmer);
-validation = (number) => !isNaN(number) && number != '';
+
+//Validation functoin validate the inputs
+const validation = (number) => !isNaN(number) && number !== '';
+
 function swapNubmer() {
-    number1 = document.getElementById('number1').value;
-    number2 = document.getElementById('number2').value;
+    let number1 = document.getElementById('number1').value;
+    let number2 = document.getElementById('number2').value;
 
     if (validation(number1) && validation(number2)) {
         getMessage('previous-number', `Before the swapping Number1: ${number1}, Number2: ${number2}`);
 
-        [number1, number2] = [number2, number1]
+        [number1, number2] = [number2, number1];
 
         getMessage('swapped-number', `After the swapping Number1: ${number1}, Number2: ${number2}`);
+    } else {
+        getMessage('previous-number', "Input must be in digit, and it can't be empty");
     }
-    else {
-        getMessage('previous-number', 'Please enter valid input');
-    }
-
-
 }
 
+//Show the output and error message.
 function getMessage(id, message) {
     document.getElementById(id).innerText = message;
 }
