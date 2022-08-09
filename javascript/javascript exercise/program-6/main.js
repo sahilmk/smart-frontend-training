6c6d3cadb03add8c81df0d967a635e078068b614
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-09 09:35:17 +0530

    #13 Practical 6 completed

--- javascript/javascript exercise/program-6/main.js
+++ javascript/javascript exercise/program-6/main.js
@@ -5,36 +5,36 @@ function findLargest(e) {
     let number2 = document.getElementById('number-2').value;
     let number3 = document.getElementById('number-3').value;
 
+    hideerror();
+
     if (validate(number1) && validate(number2) && validate(number3)) {
         number1 = Number(number1);
         number2 = Number(number2);
         number3 = Number(number3);
-        let largestNumber;
 
-        if (number1 > number2) {
-            if (number3 > number1) {
-                largestNumber = number3;
-            } else {
-                largestNumber = number1;
-            }
-        } else {
-            if (number3 > number2) {
-                largestNumber = number3;
-            } else {
-                largestNumber = number2;
-            }
-        }
+        let largestNumber = Math.max(number1, number2, number3);
 
-        getMessage('#show', `Largest number among the ${number1}, ${number2} and ${number3} is: ${largestNumber}`)
+        //Another logic for finding the largest value among three
+        // if (number1 > number2) {
+        //     if (number3 > number1) {
+        //         largestNumber = number3;
+        //     } else {
+        //         largestNumber = number1;
+        //     }
+        // } else {
+        //     if (number3 > number2) {
+        //         largestNumber = number3;
+        //     } else {
+        //         largestNumber = number2;
+        //     }
+        // }
+
+        getMessage('#show', `Largest number among the ${number1}, ${number2} and ${number3} is: ${largestNumber}`);
 
     } else {
         if (!validate(number1)) {
             getMessage('#error-1', "Value must be number and it can't be empty");
             document.getElementById('number-1').value = '';
-
-            setTimeout(function () {
-
-            }, 3000)
         }
         if (!validate(number2)) {
             getMessage('#error-2', "Value must be number and it can't be empty");
@@ -54,6 +54,14 @@ function getMessage(messageId, message) {
     document.querySelector(messageId).innerText = message;
 }
 
+//Validate the inputs
 function validate(number) {
     return number !== '' && !isNaN(Number(number));
+}
+
+//hide the error
+function hideerror() {
+    getMessage('#error-1', '');
+    getMessage('#error-2', '');
+    getMessage('#error-3', '');
 }
