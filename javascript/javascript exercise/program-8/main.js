e0558f34bcbc87f51943e6ee1fa69e4c0aa2ef7d
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-09 11:55:28 +0530

    #15 Practical 8 completed.

---
+++ javascript/javascript exercise/program-8/main.js
@@ -0,0 +1,38 @@
+document.getElementById('findFactorial').addEventListener('click', findFactorial);
+
+function findFactorial(e) {
+    let factorialNumber = document.getElementById('number').value;
+
+    getMessage('#error', '');
+
+    if (validate(factorialNumber)) {
+        let factorial = 1;
+        factorialNumber = Number(factorialNumber);
+
+        if (factorialNumber === 0) {
+            factorial = 1;
+        } else {
+            for (let i = factorialNumber; i > 0; i--) {
+                factorial *= i;
+            }
+        }
+
+        getMessage('#show', `Factorial of ${factorialNumber}! is: ${factorial}`);
+    } else {
+        getMessage('#error', "Value must be positive number and it can't be empty");
+        document.getElementById('number').value = '';
+        getMessage('#show', '');
+    }
+
+    e.preventDefault();
+}
+
+// Show the output and error messages
+function getMessage(messageId, message) {
+    document.querySelector(messageId).innerText = message;
+}
+
+//Validate the inputs
+function validate(number) {
+    return number !== '' && !isNaN(Number(number)) && Number(number) >= 0;
+}
