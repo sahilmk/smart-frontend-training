029094fe21d72c2dbc284df7e409e67574dbc1ec
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-13 18:09:16 +0530

    #22 Practical 15 completed

---
+++ javascript/javascript exercise/program-15/main.js
@@ -0,0 +1,52 @@
+const base = document.getElementById('number-1');
+const exponent = document.getElementById('number-2');
+document.getElementById('submit').addEventListener('click', findPower);
+
+function findPower(e) {
+    let number1 = base.value;
+    let number2 = exponent.value;
+    hideerror();
+
+    if (validate(number1) && validate(number2)) {
+        number1 = Number(number1);
+        number2 = Number(number2);
+
+        let power = Math.pow(number1, number2);
+
+        getMessage('#show', `${number1} ^ ${number2} : ${power}`);
+        clearInputs();
+    } else {
+        console.log(validate(number1), validate(number2));
+        if (!validate(number1)) {
+            getMessage('#error-1', "Value must be number.");
+        }
+        if (!validate(number2)) {
+            getMessage('#error-2', "Value must be number.");
+        }
+        getMessage('#show', '');
+    }
+
+    e.preventDefault();
+}
+
+// Show the output and error messages
+function getMessage(messageId, message) {
+    document.querySelector(messageId).innerText = message;
+}
+
+//Validate the inputs
+function validate(number) {
+    return number !== '' && !isNaN(Number(number));
+}
+
+//hide the error
+function hideerror() {
+    getMessage('#error-1', '');
+    getMessage('#error-2', '');
+}
+
+//Clear the inputs
+function clearInputs() {
+    base.value = '';
+    exponent.value = '';
+}
