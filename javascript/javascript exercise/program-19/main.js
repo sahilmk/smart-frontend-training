4bc5aa5d1bc4f8f2ae4fbd79891bd3b287fc7371
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-13 22:52:57 +0530

    #26 Practical 19 completed.

---
+++ javascript/javascript exercise/program-19/main.js
@@ -0,0 +1,47 @@
+const input = document.getElementById('number');
+document.getElementById('submit').addEventListener('click', findArmstrong);
+
+function findArmstrong(e) {
+    let number = input.value
+
+    if (validate(number)) {
+        let reminder = 0;
+        let sum = 0;
+
+        //Converting the string to number and finding the lenght of number
+        number = Number(number);
+        const numberLength = number.toString().length;
+
+        while (number != 0) {
+            reminder = number % 10;
+            sum += Math.pow(reminder, numberLength);
+            number = Math.floor(number / 10);
+        }
+
+        if (sum === Number(input.value)) {
+            getMessage('#show', `${input.value} is a armstrong`);
+        } else {
+            getMessage('#show', `${input.value} is not a armstrong`)
+        }
+
+        //Remove the error 
+        getMessage('#error', '');
+        input.value = '';
+    } else {
+        getMessage('#error', "Value must be positive integer.");
+        getMessage('#show', '');
+    }
+
+    e.preventDefault();
+}
+
+// Show the output and error messages
+function getMessage(messageId, message) {
+    document.querySelector(messageId).innerText = message;
+}
+
+//Validate the inputs
+function validate(number) {
+    let newNumber = Number(number)
+    return number !== '' && !isNaN(newNumber) && Number.isInteger(newNumber) && newNumber >= 1;
+}
