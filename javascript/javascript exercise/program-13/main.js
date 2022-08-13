604aaa1436bb5ebcaae915f76b1998d68ab20a76
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-13 11:32:40 +0530

    #20 Practical 13 completed.

---
+++ javascript/javascript exercise/program-13/main.js
@@ -0,0 +1,35 @@
+const input = document.getElementById('number');
+document.getElementById('findDigit').addEventListener('click', findDigit);
+
+function findDigit(e) {
+    if (validate(input.value)) {
+        let totalDigit = 0;
+        let number = Number(input.value);
+
+        while (number >= 1) {
+            number = number / 10;
+            totalDigit += 1;
+        }
+
+        //Display the output and remove the error 
+        getMessage('#show', `Total number of digit in  ${input.value} is: ${totalDigit}`);
+        getMessage('#error', '');
+        input.value = '';
+    } else {
+        getMessage('#error', "Value must be positive integer.");
+        getMessage('#show', '');
+    }
+
+    e.preventDefault();
+}
+
+// Show the output and error messages
+function getMessage(messageId, message) {
+    document.querySelector(messageId).innerText = message;
+}
+
+//Validate the inputs
+function validate(number) {
+    let newNumber = Number(number)
+    return number !== '' && !isNaN(newNumber) && newNumber >= 0 && Number.isInteger(newNumber);
+}
