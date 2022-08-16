064a25692409906edc6d36ea7dad9adb05260eb5
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-16 18:11:57 +0530

    #34 Practical 27 completed

---
+++ javascript/javascript exercise/program-27/main.js
@@ -0,0 +1,34 @@
+document.getElementById('findReverse').addEventListener('click', findReverse);
+
+function findReverse(e) {
+    const string = document.getElementById('string').value;
+
+    showMessagee('#error', '');
+
+    if (string !== "") {
+
+        const outputString = reverseString(string);
+
+        showMessagee('#show', `Original string: ${string} <br> Reverse string: ${outputString}`);
+        document.getElementById('string').value = '';
+    } else {
+        showMessagee('#error', "Please enter a string");
+        showMessagee('#show', '');
+    }
+
+    e.preventDefault();
+}
+
+//Recursive function for calculating the factorial of the number.
+function reverseString(string) {
+    if (string === "") {
+        return "";
+    } else {
+        return reverseString(string.substr(1)) + string.charAt(0);
+    }
+}
+
+// Show the output and error messages
+function showMessagee(messageId, message) {
+    document.querySelector(messageId).innerHTML = message;
+}
