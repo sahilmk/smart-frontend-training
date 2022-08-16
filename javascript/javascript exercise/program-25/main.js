fad14d27f17b5359cb192beefb9adc45b14425a1
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-16 17:10:41 +0530

    #32 Practical 25 completed.

---
+++ javascript/javascript exercise/program-25/main.js
@@ -0,0 +1,40 @@
+document.getElementById('findFactorial').addEventListener('click', findFactorial);
+
+function findFactorial(e) {
+    let factorialNumber = document.getElementById('number').value;
+
+    showMessagee('#error', '');
+
+    if (validate(factorialNumber)) {
+        factorialNumber = Number(factorialNumber);
+
+        let factorial = calculateFactorial(factorialNumber);
+
+        showMessagee('#show', `Factorial of ${factorialNumber}! is: ${factorial}`);
+        document.getElementById('number').value = '';
+    } else {
+        showMessagee('#error', "Value must be positive number and it can't be empty");
+        showMessagee('#show', '');
+    }
+
+    e.preventDefault();
+}
+
+//Recursive function for calculating the factorial of the number.
+function calculateFactorial(number) {
+    if (number === 0) {
+        return 1;
+    }
+
+    return number * calculateFactorial(number - 1);
+}
+
+// Show the output and error messages
+function showMessagee(messageId, message) {
+    document.querySelector(messageId).innerText = message;
+}
+
+//Validate the inputs
+function validate(number) {
+    return number !== '' && !isNaN(Number(number)) && Number(number) >= 0;
+}
