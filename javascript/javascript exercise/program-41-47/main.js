6636dd31610b6e268b0c244da9a87cc4f9fa673d
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-29 14:19:13 +0530

    #48 Done the changes according to the review

--- javascript/javascript exercise/program-41-47/main.js
+++ javascript/javascript exercise/program-41-47/main.js
@@ -1,11 +1,24 @@
 const showAnswer = document.getElementById('show');
-document.getElementById('sortbyid').addEventListener('click', sortById);
-document.getElementById('sortbyfn').addEventListener('click', sortByFn);
-document.getElementById('sortbyln').addEventListener('click', sortByLn);
-document.getElementById('sortbygend').addEventListener('click', sortByGend);
-document.getElementById('sortbyage').addEventListener('click', sortByAge);
+const sortIdButton = document.getElementById('sortbyid');
+sortIdButton.keyName = 'id';
+const sortFnButton = document.getElementById('sortbyfn');
+sortFnButton.keyName = 'firstName';
+const sortLnButton = document.getElementById('sortbyln');
+sortLnButton.keyName = 'lastName';
+const sortGenderButton = document.getElementById('sortbygend');
+sortGenderButton.keyName = 'gender';
+const sortAgeButton = document.getElementById('sortbyage');
+sortAgeButton.keyName = 'age';
+
+sortIdButton.addEventListener('click', sort);
+sortFnButton.addEventListener('click', sort);
+sortLnButton.addEventListener('click', sort);
+sortGenderButton.addEventListener('click', sort);
+sortAgeButton.addEventListener('click', sort);
 document.getElementById('filterinput').addEventListener('click', filterArray);
 document.getElementById('getkey').addEventListener('click', getKey);
+
+
 
 //Person object
 const person = [
@@ -14,104 +27,42 @@ const person = [
         firstName: "John",
         lastName: "Doe",
         gender: "Male",
-        age: 27
+        age: '27'
     },
     {
         id: "2",
-        firstName: "person1",
-        lastName: "surname1",
+        firstName: "Person1",
+        lastName: "Surname1",
         gender: "Female",
-        age: 25
+        age: '25'
     },
     {
         id: "4",
         firstName: "Sahil",
         lastName: "Kanjariya",
         gender: "Male",
-        age: 20
+        age: '20'
     },
     {
         id: "3",
-        firstName: "person2",
-        lastName: "surname2",
+        firstName: "Person2",
+        lastName: "Surname2",
         gender: "Male",
-        age: 45
+        age: '45'
     }
 ]
 
 document.addEventListener('DOMContentLoaded', showInitialArray(person));
 
-//sort array by object id
-function sortById() {
+//Sort array
+function sort(e) {
+    const attribute = e.currentTarget.keyName;
+
     function compare(a, b) {
-        if (a.id < b.id) {
+        if (a[attribute] < b[attribute]) {
             return -1;
         }
-        if (a.id > b.id) {
-            return 1;
-        }
-        return 0;
-    }
-
-    person.sort(compare);
-    showInitialArray(person);
-}
-
-//sort array by first name
-function sortByFn() {
-    function compare(a, b) {
-        if (a.firstName < b.firstName) {
-            return -1;
-        }
-        if (a.firstName > b.firstName) {
-            return 1;
-        }
-        return 0;
-    }
-
-    person.sort(compare);
-    showInitialArray(person);
-}
-
-//sort array by last name
-function sortByLn() {
-    function compare(a, b) {
-        if (a.lastName < b.lastName) {
-            return -1;
-        }
-        if (a.lastName > b.lastName) {
-            return 1;
-        }
-        return 0;
-    }
-
-    person.sort(compare);
-    showInitialArray(person);
-}
-
-//sort array by gender
-function sortByGend() {
-    function compare(a, b) {
-        if (a.gender < b.gender) {
-            return -1;
-        }
-        if (a.gender > b.gender) {
-            return 1;
-        }
-        return 0;
-    }
-
-    person.sort(compare);
-    showInitialArray(person);
-}
-
-//sort array by age
-function sortByAge() {
-    function compare(a, b) {
-        if (a.age < b.age) {
-            return -1;
-        }
-        if (a.age > b.age) {
+        if (a[attribute] > b[attribute]) {
             return 1;
         }
         return 0;
@@ -149,64 +100,19 @@ function filterArray() {
             let output = '';
             switch (sortMethod) {
                 case 'id':
-                    for (let i = 0; i < 4; i++) {
-                        if (person[i].id === filterValue) {
-                            output += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age} <br>`;
-                        }
-                    }
-                    if (output === '') {
-                        output = 'Not Found :(';
-                    }
-
-                    showAnswer.innerHTML = output;
+                    filter('id', filterValue);
                     break;
                 case 'fname':
-                    for (let i = 0; i < 4; i++) {
-                        if ((person[i].firstName).toLowerCase() === filterValue) {
-                            output += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age} <br>`;
-                        }
-                    }
-                    if (output === '') {
-                        output = 'Not Found :(';
-                    }
-
-                    showAnswer.innerHTML = output;
+                    filter('fname', filterValue);
                     break;
                 case 'lname':
-                    for (let i = 0; i < 4; i++) {
-                        if ((person[i].lastName).toLowerCase() === filterValue) {
-                            output += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age} <br>`;
-                        }
-                    }
-                    if (output === '') {
-                        output = 'Not Found :(';
-                    }
-
-                    showAnswer.innerHTML = output;
+                    filter('lname', filterValue);
                     break;
                 case 'gender':
-                    for (let i = 0; i < 4; i++) {
-                        if ((person[i].gender).toLowerCase() === filterValue) {
-                            output += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age} <br>`;
-                        }
-                    }
-                    if (output === '') {
-                        output = 'Not Found :(';
-                    }
-
-                    showAnswer.innerHTML = output;
+                    filter('gender', filterValue);
                     break;
                 case 'age':
-                    for (let i = 0; i < 4; i++) {
-                        if (String(person[i].age) === filterValue) {
-                            output += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age} <br>`;
-                        }
-                    }
-                    if (output === '') {
-                        output = 'Not Found :(';
-                    }
-
-                    showAnswer.innerHTML = output;
+                    filter('age', filterValue);
                     break;
             }
         } else {
@@ -217,12 +123,30 @@ function filterArray() {
     }
 }
 
+//Filter the array according to the 
+function filter(filterKeyName, filterValue) {
+    let output = '';
+
+    for (let i = 0; i < 4; i++) {
+        if (person[i][filterKeyName].toLowerCase() === filterValue) {
+            output += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age} <br>`;
+        }
+    }
+    if (output === '') {
+        output = 'Not Found :(';
+    }
+
+    showAnswer.innerHTML = output;
+}
+
 //Display the output
 function showInitialArray(person) {
     let string = '';
+
     for (let i = 0; i < 4; i++) {
         string += `Id: ${person[i].id}, Fristname: ${person[i].firstName}, Lastname: ${person[i].lastName}, Gender: ${person[i].gender}, Age: ${person[i].age}<br>`;
     }
+
     showAnswer.innerHTML = string;
 }
 
