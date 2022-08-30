6ee95a98bf7743f20dbe03ad3e9cab833baf7a2b
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-08-30 11:55:48 +0530

    #45 Done the changes according to the review

--- javascript/javascript exercise/program-38/main.js
+++ javascript/javascript exercise/program-38/main.js
@@ -1,22 +1,22 @@
-const input1 = document.getElementById('string-1');
-const input2 = document.getElementById('string-2');
+const inputOne = document.getElementById('stringOne');
+const inputTwo = document.getElementById('stringTwo');
 document.getElementById('submit').addEventListener('click', concatenateString);
 
 function concatenateString(e) {
-    const string1 = input1.value;
-    const string2 = input2.value;
+    const string1 = inputOne.value;
+    const string2 = inputTwo.value;
     hideerror();
 
     if (validate(string1) && validate(string2)) {
-
         showMessage('#show', `First string: ${string1}<br> Second string: ${string2}<br> Concatenated string: ${string1 + string2}`);
+
         clearInputs();
     } else {
         if (!validate(string1)) {
-            showMessage('#error-1', "Please enter a string.");
+            showMessage('#errorOne', "Please enter a string.");
         }
         if (!validate(string2)) {
-            showMessage('#error-2', "Please enter a string.");
+            showMessage('#errorTwo', "Please enter a string.");
         }
         showMessage('#show', '');
     }
@@ -31,17 +31,18 @@ function showMessage(messageId, message) {
 
 //Validate the inputs
 function validate(string) {
-    return string !== '';
+    const regex = /^\s+$/
+    return string !== '' && !regex.test(string);
 }
 
 //hide the error
 function hideerror() {
-    showMessage('#error-1', '');
-    showMessage('#error-2', '');
+    showMessage('#errorOne', '');
+    showMessage('#errorTwo', '');
 }
 
 //Clear the inputs
 function clearInputs() {
-    input1.value = '';
-    input2.value = '';
+    inputOne.value = '';
+    inputTwo.value = '';
 }
