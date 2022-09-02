afdc414ca67b0385c399b857a41bd7c3742ea374
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-09-02 15:57:35 +0530

    #39 Done the changes according to the review

--- javascript/javascript exercise/program-32/main.js
+++ javascript/javascript exercise/program-32/main.js
@@ -2,56 +2,56 @@ document.getElementById('submit').addEventListener('click', multiplytMatrix);
 const showMatrix1 = document.getElementById('showMatrix1');
 const showMatrix2 = document.getElementById('showMatrix2');
 const showOutput = document.getElementById('answer');
-const row = document.getElementById('row');
-const column = document.getElementById('column');
+const rowOne = document.getElementById('rowOne');
+const columnOne = document.getElementById('columnOne');
+const rowTwo = document.getElementById('rowTwo');
+const columnTwo = document.getElementById('columnTwo');
 
 let matrix1, matrix2;
 
 //Add the two matrix
 function multiplytMatrix(e) {
-    let rowNumber = row.value;
-    let columnNumber = column.value;
-
+    const matrixOneRow = rowOne.value;
+    const matrixOneColumn = columnOne.value;
+    const matrixTwoRow = rowTwo.value;
+    const matrixTwoColumn = columnTwo.value;
     clearError();
-    if (validate(rowNumber) && validate(columnNumber) && rowNumber === columnNumber) {
-        let outputMatrix = new Array(rowNumber);
+
+    if (validate(matrixOneRow) && validate(matrixOneColumn) && validate(matrixTwoRow) && validate(matrixTwoColumn) && matrixOneColumn === matrixTwoRow) {
+        let outputMatrix = new Array(matrixOneRow);
         let output = '';
 
-        createMatrix();
+        createMatrix(1, matrixOneRow, matrixOneColumn);
+        createMatrix(2, matrixTwoRow, matrixTwoColumn);
 
-        for (let i = 0; i < rowNumber; i++) {
-            outputMatrix[i] = new Array(columnNumber);
-        }
+        for (let i = 0; i < matrixOneRow; i++) {
+            outputMatrix[i] = new Array(matrixTwoColumn);
+            output += '[';
 
-        for (let i = 0; i < rowNumber; i++) {
-            output += `[`;
-            for (let j = 0; j < columnNumber; j++) {
+            for (let j = 0; j < matrixTwoColumn; j++) {
                 let ans = 0;
 
-                for (let k = 0; k < columnNumber; k++) {
+                for (let k = 0; k < matrixOneColumn; k++) {
                     ans += matrix1[i][k] * matrix2[k][j];
                 }
 
                 outputMatrix[i][j] = ans;
-                output += `${outputMatrix[i][j]} `;
+                output += outputMatrix[i][j];
             }
-            output += `]<br>`;
-        }
 
+            output += ']<br>';
+        }
 
         showOutput.innerHTML = output;
-        row.value = '';
-        column.value = '';
-
+        clearInput();
     } else {
-        if (validate(rowNumber)) {
-            showMessage('#error-1', 'Please enter positive number greater than one');
-        }
-        if (validate(columnNumber)) {
-            showMessage('#error-2', 'Please enter positive number greater than one');
-        }
-        if (rowNumber !== columnNumber) {
-            showMessage('#error-2', 'Both value must be same.');
+        showError('#error-1', matrixOneRow);
+        showError('#error-2', matrixOneColumn);
+        showError('#error-3', matrixTwoRow);
+        showError('#error-4', matrixTwoColumn);
+
+        if (matrixOneColumn !== matrixTwoRow) {
+            showMessage('#error-4', 'The number of column in matrix 1 and number of row in matrix 2 must be same');
         }
 
         clearOutput();
@@ -59,49 +59,43 @@ function multiplytMatrix(e) {
     e.preventDefault();
 }
 
-function createMatrix() {
+function createMatrix(number, matrixRow, matrixColumns) {
+    let showmat = '';
+    if (number === 1) {
+        matrix1 = new Array(matrixRow);
 
-    let rows = Number(row.value);
-    let columns = Number(column.value);
+        //Create matrix
+        for (let i = 0; i < matrixRow; i++) {
+            showmat += '[';
+            matrix1[i] = new Array(matrixColumns);
 
-    matrix1 = new Array(rows);
-    matrix2 = new Array(rows);
+            for (let j = 0; j < matrixColumns; j++) {
+                matrix1[i][j] = i + 1;
+                showmat += matrix1[i][j];
+            }
+            showmat += ']<br>';
 
-    //Create matrix
-    for (let i = 0; i < rows; i++) {
-        matrix1[i] = new Array(columns);
-        matrix2[i] = new Array(columns);
-
-        for (let j = 0; j < columns; j++) {
-            matrix1[i][j] = i + 1;
-            matrix2[i][j] = i + 1;
         }
-    }
 
-    //Display the matrix
-    let showmat1 = '';
-    let showmat2 = '';
+        showMatrix1.innerHTML = showmat;
+    } else {
+        matrix2 = new Array(matrixRow);
 
-    for (let i = 0; i < rows; i++) {
-        showmat1 += `[`;
-        for (let j = 0; j < columns; j++) {
-            showmat1 += `${matrix1[i][j]} `;
+        //Create matrix
+        for (let i = 0; i < matrixRow; i++) {
+            showmat += '[';
+            matrix2[i] = new Array(matrixColumns);
+
+            for (let j = 0; j < matrixColumns; j++) {
+                matrix2[i][j] = i + 1;
+                showmat += matrix2[i][j];
+            }
+            showmat += ']<br>';
+
         }
-        showmat1 += `]`;
-        showmat1 += `<br>`;
-    }
 
-    for (let i = 0; i < rows; i++) {
-        showmat2 += `[`;
-        for (let j = 0; j < columns; j++) {
-            showmat2 += `${matrix2[i][j]} `;
-        }
-        showmat2 += `]`;
-        showmat2 += `<br>`;
+        showMatrix2.innerHTML = showmat;
     }
-
-    showMatrix1.innerHTML = showmat1;
-    showMatrix2.innerHTML = showmat2;
 }
 
 // Show the output and error messages
@@ -109,17 +103,37 @@ function showMessage(messageId, message) {
     document.querySelector(messageId).innerHTML = message;
 }
 
-function validate(number) {
-    return number !== '' && Number(number) >= 1 && !isNaN(number);
+//Show the error in perticular input if any is there.
+function showError(id, value) {
+    if (!validate(value)) {
+        showMessage(id, 'Please enter positive number greater than one');
+    }
 }
 
+//Validate the input
+function validate(number) {
+    return number !== '' && Number(number) >= 1 && Number.isInteger(Number(number));
+}
+
+//Clear the error
 function clearError() {
     document.getElementById('error-1').innerHTML = '';
     document.getElementById('error-2').innerHTML = '';
+    document.getElementById('error-3').innerHTML = '';
+    document.getElementById('error-4').innerHTML = '';
 }
 
+//Clear the output
 function clearOutput() {
     showMatrix1.innerHTML = '';
     showMatrix2.innerHTML = '';
     showOutput.innerHTML = '';
+}
+
+//Clear the input
+function clearInput() {
+    rowOne.value = '';
+    columnOne.value = '';
+    rowTwo.value = '';
+    columnTwo.value = '';
 }
