64e6475c957991fa6fcdf50a5861746a1c6a7d0e
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2022-09-02 18:01:48 +0530

    #47 Remove the space loop

--- javascript/javascript exercise/program-40/main.js
+++ javascript/javascript exercise/program-40/main.js
@@ -1,67 +1,75 @@
 document.getElementById('submit').addEventListener('click', showPatterns);
 const numberOfRow = document.getElementById('row');
 const showOutput = document.getElementById('show-1');
-const selectedPattern = document.getElementById('select1');
+const selectedPattern = document.getElementById('select');
+const error = document.getElementById('error');
 
 let outputMessage = '';
 
 function showPatterns(e) {
-    const rowNumber = numberOfRow.value;
-    outputMessage = '';
-    const pyramidType = selectedPattern.value;
+    let rowNumber = numberOfRow.value;
 
-    switch (pyramidType) {
-        case 'halfstartriangle':
-            outputMessage = '<h3>Half * Triangle</h3>';
-            halfPyramid(rowNumber, true, true);
-            break;
-        case 'halfdigittriangle':
-            outputMessage += '<h3> Half Digit Triangle</h3>';
-            halfPyramid(rowNumber, false, true);
-            break;
-        case 'halfalphabettriangle':
-            outputMessage += '<h3> Half Alphabet Triangle</h3>';
-            halfAlphabatPyramid(rowNumber);
-            break;
-        case 'reversehalfstar':
-            outputMessage += '<h3> Half Reverse * Triangle</h3>';
-            halfPyramid(rowNumber, true, false);
-            break;
-        case 'reversehalfdigit':
-            outputMessage += '<h3> Half Reverse Digit Triangle</h3>';
-            halfPyramid(rowNumber, false, false);
-            break;
-        case 'startriangle':
-            outputMessage += '<h3> Full * Triangle</h3>';
-            fullPyramid(rowNumber, true);
-            break;
-        case 'digittriangle':
-            outputMessage += '<h3> Full Digit Triangle</h3>';
-            digitPyramid(rowNumber);
-            break;
-        case 'reversestartriangle':
-            outputMessage += '<h3> Full Reverse * Triangle</h3>';
-            fullPyramid(rowNumber, false);
-            break;
-        case 'pascaltriangle':
-            outputMessage += '<h3> Pascal Triangle</h3>';
-            pascalTriangle(rowNumber);
-            break;
-        case 'incrementnumbertriangle':
-            outputMessage += '<h3> Increasing Number Triangle</h3>';
-            increasingNumberTriangle(rowNumber);
-            break;
+    if (validate(rowNumber)) {
+        const pyramidType = selectedPattern.value;
+        rowNumber = Number(rowNumber);
+        outputMessage = '';
+
+        switch (pyramidType) {
+            case 'halfstartriangle':
+                outputMessage = '<h3>Half * Triangle</h3>';
+                halfPyramid(rowNumber, 'star', 'straight');
+                break;
+            case 'halfdigittriangle':
+                outputMessage += '<h3> Half Digit Triangle</h3>';
+                halfPyramid(rowNumber, 'digit', 'straight');
+                break;
+            case 'halfalphabettriangle':
+                outputMessage += '<h3> Half Alphabet Triangle</h3>';
+                halfAlphabatPyramid(rowNumber);
+                break;
+            case 'reversehalfstar':
+                outputMessage += '<h3> Half Reverse * Triangle</h3>';
+                halfPyramid(rowNumber, 'star', 'reverse');
+                break;
+            case 'reversehalfdigit':
+                outputMessage += '<h3> Half Reverse Digit Triangle</h3>';
+                halfPyramid(rowNumber, 'digit', 'reverse');
+                break;
+            case 'startriangle':
+                outputMessage += '<h3> Full * Triangle</h3>';
+                fullPyramid(rowNumber, 'straight');
+                break;
+            case 'digittriangle':
+                outputMessage += '<h3> Full Digit Triangle</h3>';
+                digitPyramid(rowNumber);
+                break;
+            case 'reversestartriangle':
+                outputMessage += '<h3> Full Reverse * Triangle</h3>';
+                fullPyramid(rowNumber, 'reverse');
+                break;
+            case 'pascaltriangle':
+                outputMessage += '<h3> Pascal Triangle</h3>';
+                pascalTriangle(rowNumber);
+                break;
+            case 'incrementnumbertriangle':
+                outputMessage += '<h3> Increasing Number Triangle</h3>';
+                increasingNumberTriangle(rowNumber);
+                break;
+        }
+
+        showOutput.innerHTML = outputMessage;
+    } else {
+        error.innerText = 'Please enter positive integer';
     }
 
-    showOutput.innerHTML = outputMessage;
     e.preventDefault();
 }
 
 //Halft pyramid for digit and *
 function halfPyramid(rowNumber, message, direction) {
     for (let row = 1; row <= rowNumber; row++) {
-        for (let column = 1; column <= (direction ? row : ((rowNumber - row) + 1)); column++) {
-            outputMessage += (message ? '*' : column);
+        for (let column = 1; column <= (direction === 'straight' ? row : ((rowNumber - row) + 1)); column++) {
+            outputMessage += (message === 'star' ? '*' : column);
         }
 
         outputMessage += '<br>';
@@ -82,11 +90,9 @@ function halfAlphabatPyramid(rowNumber) {
 //Fullpyramid of star
 function fullPyramid(rowNumber, direction) {
     for (let row = 1; row <= rowNumber; row++) {
-        for (let space = 1; space <= Number(direction ? rowNumber - row : (row - 1)); space++) {
-            outputMessage += '&nbsp&nbsp ';
-        }
+        outputMessage += '&nbsp'.repeat(Number(direction === 'straight' ? rowNumber - row : (row - 1)) * 3);
 
-        for (let column = 0; column < Number(direction ? ((2 * row) - 1) : ((2 * (rowNumber - row)) + 1)); column++) {
+        for (let column = 0; column < Number(direction === 'straight' ? ((2 * row) - 1) : ((2 * (rowNumber - row)) + 1)); column++) {
             outputMessage += " *";
         }
 
@@ -99,10 +105,7 @@ function digitPyramid(rowNumber) {
     let count = 0, count1 = 0;
 
     for (let row = 1; row <= rowNumber; row++) {
-        for (let space = 1; space <= rowNumber - row; space++) {
-            outputMessage += '&nbsp&nbsp';
-            count++;
-        }
+        outputMessage += '&nbsp'.repeat((rowNumber - row) * 2);
 
         for (let column = 0; column < (2 * row) - 1; column++) {
             if (count <= rowNumber - 1) {
@@ -124,9 +127,7 @@ function pascalTriangle(rowNumber) {
     let coef = 1;
 
     for (let row = 0; row < rowNumber; row++) {
-        for (let space = 1; space < rowNumber - row; space++) {
-            outputMessage += '&nbsp';
-        }
+        outputMessage += '&nbsp'.repeat((rowNumber - row));
 
         for (let column = 0; column <= row; column++) {
             if (column == 0 || row == 0) {
@@ -156,4 +157,8 @@ function increasingNumberTriangle(rowNumber) {
 
         outputMessage += '<br>';
     }
+}
+
+function validate(number) {
+    return number !== '' && Number.isInteger(Number(number)) && Number(number) > 0;
 }
